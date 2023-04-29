dacd8b0a61e1befdf48c66afb054df5738e1fa1e
jungyungee <86039030+jungyungee@users.noreply.github.com>
2023-04-30 05:09:21 +0900

    체크 박스 앞쪽으로 옮기기 성공

--- input.js
+++ input.js
@@ -7,21 +7,21 @@ function addTodo(){
         alert("할 일이 입력되지 않았습니다!")   //아무것도 입력하지 않았을 때 경고창
     }
     else{
-        // 추가할 li element 생성 (할일 리스트에 동적으로 추가)
+        // 체크박스, 할일 내용, 삭제 버튼을 포함할 li 생성
         let list = document.createElement('li');
-        // 할일 완료 체크 박스
+        // 체크박스, 할일 내용, 삭제 버튼 생성
         let chk = document.createElement("input");
         chk.type="checkbox"
-        // 삭제 버튼
+        let todo = document.createElement("div");
         let del = document.createElement('button');
-
-        // 입력받은 값을 li에 담기
-        list.innerHTML = newTodo.value;
-        // li 내용을 todoList ul에 자식으로 추가
-        document.getElementById('todoList').appendChild(list);
 
         // li에 체크박스 추가
         list.appendChild(chk);
+
+        // 입력받은 값을 todo에 담기, li에 todo 추가
+        todo.innerHTML = newTodo.value;
+        list.appendChild(todo);
+
         // li에 삭제버튼 추가
         list.appendChild(del);
         del.innerText = "x";
@@ -29,10 +29,13 @@ function addTodo(){
         del.style.background="none"
         del.style.cursor = "pointer";
 
-        //6. 삭제버튼 클릭 시, 삭제 이벤트 실행
+        // li 내용들을 todoList ul에 자식으로 추가
+        document.getElementById('todoList').appendChild(list);
+
+        // 삭제버튼 클릭 시, 삭제 이벤트 실행
         del.addEventListener("click",deleteLi);
 
-        //할일 완료
+        //할일 완료 기능
         chk.addEventListener("change", () => {
             if (chk.checked == true ){
                 list.style.textDecoration = "line-through";
