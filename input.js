afbbe734ddefa80878656db8322b5040126d754f
jungyungee <86039030+jungyungee@users.noreply.github.com>
2023-05-09 18:40:59 +0900

    기존에 입력된 값 리스트에 나타나는 기능:printSaved 추가 및 할일 완료 부분 함수로 분리

--- input.js
+++ input.js
@@ -2,8 +2,11 @@ let newTodo = document.getElementById("newTodo");  //입력된 할일
 const button = document.querySelector("button"); //버튼
 let todoList = document.getElementById('todoList') //ul
 
+printSaved();
+
 //할일 추가 기능
 function addTodo(){
+    saveStorage();
     if(newTodo.value == false){
         alert("할 일이 입력되지 않았습니다!")   //아무것도 입력하지 않았을 때 경고창
     }
@@ -32,26 +35,26 @@ function addTodo(){
 
         // li 내용들을 todoList ul에 자식으로 추가
         todoList.appendChild(list);
-
         // 삭제버튼 클릭 시, 삭제 이벤트 실행
         del.addEventListener("click",deleteLi);
-
         //할일 완료 
-        chk.addEventListener("change", () => {
-            if (chk.checked == true ){
-                todo.style.textDecoration = "line-through";
-                todo.style.color = "gray"; 
-            }
-            else {
-                todo.style.textDecoration = "none";
-                todo.style.color = "#34393A";
-            }
-        })
-
-        saveStorage();
+        chk.addEventListener("change", checkLi);
 
         // 입력된 내용은 입력 창에서 사라지게
         newTodo.value = null;
+    }
+}
+
+//할일 완료 표시 함수 (체크박스)
+function checkLi(){
+    if (this.checked == true ){
+        todo = this.parentElement;
+        todo.style.textDecoration = "line-through";
+        todo.style.color = "gray"; 
+    }
+    else {
+        todo.style.textDecoration = "none";
+        todo.style.color = "#34393A";
     }
 }
 
@@ -74,5 +77,49 @@ function saveStorage(){
         // 기존 저장된 값이 없으면 배열 만들기
         todos = [newTodo.value];
         localStorage.setItem("todoLocal", JSON.stringify(todos));
+    }
+}
+
+// 로컬 스토리지에 저장되어 있던 값 관련 함수
+function printSaved(){
+    let todos = JSON.parse(localStorage.getItem("todoLocal"));
+
+    // 
+    if (todos !== null){
+        todos.forEach(e => {
+            // 체크박스, 할일 내용, 삭제 버튼을 포함할 list 생성
+        let list = document.createElement('li');
+        // 체크박스, 할일 내용, 삭제 버튼 생성
+        let chk = document.createElement("input");
+        chk.type="checkbox";
+        let todo = document.createElement("div");
+        let del = document.createElement('button');
+    
+        // li에 체크박스 추가
+        list.appendChild(chk);
+    
+        // 입력받은 값을 todo에 담기, li에 todo 추가
+        todo.innerHTML = e;
+        list.appendChild(todo);
+    
+        // li에 삭제버튼 추가
+        list.appendChild(del);
+        del.innerText = "x";
+        del.style.border = "none";
+        del.style.background="none"
+        del.style.cursor = "pointer";
+    
+        // li 내용들을 todoList ul에 자식으로 추가
+        todoList.appendChild(list);
+
+        // 삭제버튼 클릭 시, 삭제 이벤트 실행
+        del.addEventListener("click", deleteLi);
+        
+        //할일 완료 
+        chk.addEventListener("change", checkLi);
+    });
+    }
+    else {
+        return;
     }
 }
