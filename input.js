18aaa9f1805c815623d27df8b3b5f962f4fafaf2
jungyungee <86039030+jungyungee@users.noreply.github.com>
2023-05-09 12:34:47 +0900

    로컬 스토리지 저장 기능 추가 완료

--- input.js
+++ input.js
@@ -1,5 +1,6 @@
 let newTodo = document.getElementById("newTodo");  //입력된 할일
 const button = document.querySelector("button"); //버튼
+let todoList = document.getElementById('todoList') //ul
 
 //할일 추가 기능
 function addTodo(){
@@ -7,7 +8,7 @@ function addTodo(){
         alert("할 일이 입력되지 않았습니다!")   //아무것도 입력하지 않았을 때 경고창
     }
     else{
-        // 체크박스, 할일 내용, 삭제 버튼을 포함할 li 생성
+        // 체크박스, 할일 내용, 삭제 버튼을 포함할 list 생성
         let list = document.createElement('li');
         // 체크박스, 할일 내용, 삭제 버튼 생성
         let chk = document.createElement("input");
@@ -30,7 +31,7 @@ function addTodo(){
         del.style.cursor = "pointer";
 
         // li 내용들을 todoList ul에 자식으로 추가
-        document.getElementById('todoList').appendChild(list);
+        todoList.appendChild(list);
 
         // 삭제버튼 클릭 시, 삭제 이벤트 실행
         del.addEventListener("click",deleteLi);
@@ -46,6 +47,20 @@ function addTodo(){
                 todo.style.color = "#34393A";
             }
         })
+
+        let todos = JSON.parse(localStorage.getItem("todoLocal"));
+
+        if (todos !== null){
+            // 기존 값이 있으면 배열에 내용 추가
+            todos.push(newTodo.value);
+            localStorage.setItem("todoLocal", JSON.stringify(todos));
+        }
+        else{
+            // 기존 저장된 값이 없으면 배열 만들기
+            todos = [newTodo.value];
+            localStorage.setItem("todoLocal", JSON.stringify(todos));
+        }
+        
 
         // 입력된 내용은 입력 창에서 사라지게
         newTodo.value = null;
